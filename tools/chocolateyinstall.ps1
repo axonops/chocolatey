938ed7e02b486c2f8875d050f8067f31964efa2d
Sergio Rua <sergio.rua@digitalis.io>
2024-08-01 17:22:05 +0100

    Choco

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/axonops/axonops-workbench-cassandra/releases/download/0.1.0-internal-20240801-brew/AxonOps.Workbench-0.1.0-internal-20240801-brew-win-x64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url64bit      = $url64
+
+  softwareName  = 'AxonOpsWorkbench*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum64    = '4a66683e69e5d8a6cfe2ee13044e4e29a49d24c06dbbf65432e1d0dfb5aaf50a'
+  checksumType64= 'sha256'
+
+  # MSI
+  silentArgs   = '/s'           # InstallShield
+}
+
+Install-ChocolateyPackage @packageArgs
+
+# code: language=powershell
