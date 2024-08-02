b78caf3e0bc24d92895014c6071b1e59f2e58c2a
Sergio Rua <sergio.rua@digitalis.io>
2024-08-02 10:10:59 +0100

    Correct syntax

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/axonops/axonops-workbench-cassandra/releases/download/%VERSION%/AxonOps.Workbench-%VERSION%-win-x64.exe'
+$url64      = 'https://github.com/axonops/axonops-workbench-cassandra/releases/download0.1.0-internal-20240802/AxonOps.Workbench-0.1.0-internal-20240802-win-x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -20,24 +20,3 @@ $packageArgs = @{
 Install-ChocolateyPackage @packageArgs
 
 # code: language=powershell﻿$ErrorActionPreference = 'Stop' # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/axonops/axonops-workbench-cassandra/releases/download/0.1.0-internal-20240802/AxonOps.Workbench-0.1.0-internal-20240802-win-x64.exe'
-
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url64bit      = $url64
-
-  softwareName  = 'AxonOpsWorkbench*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-  checksum64    = '%SHA_AMD%'
-  checksumType64= 'sha256'
-
-  # MSI
-  silentArgs   = '/s'           # InstallShield
-}
-
-Install-ChocolateyPackage @packageArgs
-
-# code: language=powershell
